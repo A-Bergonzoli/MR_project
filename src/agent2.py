88d976a1747c7efc968ec387171ab5a19714e00a
A-Bergonzoli <86882788+A-Bergonzoli@users.noreply.github.com>
2021-07-03 16:46:16 +0200

    Update agent2.py

--- src/agent2.py
+++ src/agent2.py
@@ -154,7 +154,7 @@ class Agent_2():
 
 
 if __name__ == "__main__":
-    # Instance an object of the 'rendezvous' class
+    # Instance an object of the 'Agent_2' class
     ob = Agent_2()
 
     # rate for the loop cycle (Hz)
