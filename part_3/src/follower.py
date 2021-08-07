27179598c22c15cb833906b0025817f73f778d67
A-Bergonzoli <86882788+A-Bergonzoli@users.noreply.github.com>
2021-08-07 09:42:06 +0200

    Update follower.py

--- part_3/src/follower.py
+++ part_3/src/follower.py
@@ -18,7 +18,7 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import numpy as np
 import math
 
-from stack_implementation import *
+from queue_implementation import *
 from control import *
 
 
